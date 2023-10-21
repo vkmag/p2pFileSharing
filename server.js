f441cedee72f131a384437fc64558410b0a1a7f9
Anurag Raushan <anuragraushan373@gmail.com>
2023-10-21 23:25:44 +0530

    3rd

--- server.js
+++ server.js
@@ -83,10 +83,10 @@ io.on('connection', (socket) => {
   });
 });
 
-const port = 3000;
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// const port = 3000;
+// server.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
 
 
 
